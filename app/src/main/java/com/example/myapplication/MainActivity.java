fd139e5d8dd9a548b181dcda9a6c4b742735ef21
shiray29 <shiray292002@gmail.com>
2020-05-10 18:23:45 +0300

    123

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
-    private int a=9;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnLogin, btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==btnLogin) {
+            // to do - new activity - log in
+        }
+        if (v==btnRegister) {
+            // to do - new activity - Register
+        }
+
     }
 }
