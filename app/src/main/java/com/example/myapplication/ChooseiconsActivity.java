7114c3f6c8766d3fd61b912a37bf4781272ff5ae
shiray29 <shiray292002@gmail.com>
2020-07-15 19:19:47 +0300

    GPS stage 3

--- app/src/main/java/com/example/myapplication/ChooseiconsActivity.java
+++ app/src/main/java/com/example/myapplication/ChooseiconsActivity.java
@@ -1,14 +1,65 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChooseiconsActivity extends AppCompatActivity {
+
+    private ImageView imageViewBuild, imageViewCall, imageViewClean, imageViewCompany, imageViewShop;
+    private TextView textViewPick, textViewBuild, textViewCall, textViewClean, textViewCompany, textViewShop;
+    private Button btnconfirmicon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseicons);
+        imageViewBuild = findViewById(R.id.imageView_build);
+        imageViewCall = findViewById(R.id.imageView_call);
+        imageViewClean = findViewById(R.id.imageView_clean);
+        imageViewCompany = findViewById(R.id.imageView_company);
+        imageViewShop = findViewById(R.id.imageView_shop);
+        textViewBuild = findViewById(R.id.textView_build);
+        textViewCall = findViewById(R.id.textView_call);
+        textViewClean = findViewById(R.id.textView_clean);
+        textViewCompany = findViewById(R.id.textView_company);
+        textViewShop = findViewById(R.id.textView_shop);
+        textViewPick = findViewById(R.id.textView_pick);
+        btnconfirmicon = findViewById(R.id.btn_confirmicon);
+    }
+
+    public void onClick(View V){
+        if (imageViewBuild==V) // TODO - and so on for all buttons. my question - how can we cancel?
+            // TODO - counter - ךעדכן רק בסוף
+        {   imageViewBuild.setClickable(false);
+            DatabaseReference  ref = FirebaseDatabase.getInstance().getReference().child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Profile profile = dataSnapshot.getValue(Profile.class);
+                    profile.setIsBuild(true);
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) { }
+            });
+        }
+
+        if (btnconfirmicon==V){ // TODO - add "are you sure?" pop-up
+            Intent intent_waitForRequest = new Intent(this, WaitforRequest.class);
+            startActivity(intent_waitForRequest);
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Search.java
+++ app/src/main/java/com/example/myapplication/Search.java
@@ -1,15 +1,24 @@
 package com.example.myapplication;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,18 +28,21 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 
-public class Search extends AppCompatActivity {
+public class Search extends AppCompatActivity implements OnMapReadyCallback {
 
     private DatabaseReference databaseReference;
     private ArrayList profileList;
     private GoogleMap googleMap;
+    private double thisLon, thisLat;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        googleMap = findViewById(R.id.mapView); // TODO - linking XML and java maps. this is probably wrong
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
         databaseReference= FirebaseDatabase.getInstance().getReference("users");
         final ArrayList<Profile> profileList = new ArrayList<Profile>();
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -51,30 +63,43 @@ public class Search extends AppCompatActivity {
         double tempLat;
         double tempLon;
 
+        DatabaseReference  ref = FirebaseDatabase.getInstance().getReference().child(FirebaseAuth.getInstance().getCurrentUser().getUid()); // get current user Lat & Lon
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Profile profile = dataSnapshot.getValue(Profile.class);
+                thisLat = profile.getLat();
+                thisLon = Profile.getLon();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+
         while (profileList.size() > count) { // מעלה את כל הקשישים במרחק של עד 30 ק"מ מהמשתמש
+            // TODO - רשימץ מרקרים עם קישור לקשיש
             if (profileList.get(count).isOld())
             {
              tempLat = profileList.get(count).getLat(); // hadas shall add
              tempLon = profileList.get(count).getLon();
-             if (findDistance(tempLat, tempLon, thisLat, thisLon) <=30) // TODO - ask how i access this user
+             if (findDistance(tempLat, tempLon, thisLat, thisLon) <=30)
              {
-                 if profileList.get(count).getIsBuild(){
-                     showMarker(tempLat, tempLon, buildID);
+                 if (profileList.get(count).getIsBuild()){
+                     showMarker(tempLat, tempLon, R.id.imageView_build);
              }
-                 if profileList.get(count).getClean(){
-                 showMarker(tempLat, tempLon, cleanID);
+                 if (profileList.get(count).getClean()){
+                 showMarker(tempLat, tempLon, R.id.imageView_clean);
              }
-                 if profileList.get(count).getCompany();
+                 if (profileList.get(count).getCompany());
                  {
-                 showMarker(tempLat, tempLon, companyID);
+                 showMarker(tempLat, tempLon, R.id.imageView_company);
              }
-                 if profileList.get(count).getShop();
+                 if (profileList.get(count).getShop());
                  {
-                 showMarker(tempLat, tempLon, shopID);
+                 showMarker(tempLat, tempLon, R.id.imageView_shop);
              }
-                 if profileList.get(count).getCall();
+                 if (profileList.get(count).getCall());
                  {
-                 showMarker(tempLat, tempLon, callID);
+                 showMarker(tempLat, tempLon, R.id.imageView_call);
              }
              }
             }
@@ -97,5 +122,33 @@ public class Search extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng point = new LatLng(thisLat, thisLon);
+        this.googleMap.addMarker(new MarkerOptions().position(point));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(point));
+    }
 
+
+    // דוגמא
+
+    public void onClick(View V){
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.popupvul);
+        Button accept = dialog.findViewById(R.id.btn_accept);
+        Button decline = dialog.findViewById(R.id.btn_decline);
+        accept.setOnClickListener((View.OnClickListener) this);
+        decline.setOnClickListener((View.OnClickListener) this);
+        if (V==accept){
+            dialog.cancel();
+            // TODO - notification;
+        }
+
+        // dialog.show
+
+
+
+    }
 }
